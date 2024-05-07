b0a0dc7aeadc8230b6af45ff15925232f9c913e2
NeneNafe <nenechiwaya@gmail.com>
2024-05-07 16:06:40 +0200

    a class FIFOCache that inherits from BaseCaching and is a caching system

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+"""a class FIFOCache that inherits from BaseCaching"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """inherits from BaseCaching and is a caching system"""
+    def __init__(self):
+        """ Initialize the cache and an insertion order list"""
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """assign to the dictionary self.cache_data"""
+        if key and item:
+            if self.cache_data.get(key):
+                self.queue.remove(key)
+            self.queue.append(key)
+            self.cache_data[key] = item
+            if len(self.queue) > self.MAX_ITEMS:
+                delete = self.queue.pop(0)
+                self.cache_data.pop(delete)
+                print('DISCARD : {}'.format(delete))
+
+    def get(self, key):
+        """returns the value in self.cache_data linked to key"""
+        return self.cache_data.get(key)
